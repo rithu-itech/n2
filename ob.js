802ec45cc9afae001e08ed1c24e971e89a7cffa6
rithu-itech <srithu98@gmail.com>
2024-01-28 09:00:06 +0530

    2nd task

---
+++ ob.js
@@ -0,0 +1,8 @@
+const student = {
+    name:"Sharp",
+    age:"18",
+    study(){
+        console.log("Hi I am " + this.name + " of age " + this.age,"studying node js")
+    }
+}
+student.study()
---
+++ play.js
@@ -0,0 +1,4 @@
+const product=(a,b) => {
+    return a*b
+}
+console.log(product(2,3))
